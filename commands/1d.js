2e95b9f316a8c279d2afb38f177255e1756d59b4
FearTheRenegade <50032027+FearTheRenegade@users.noreply.github.com>
2019-04-27 21:21:44 -0400

    Added number checking and removed broken weighted die rolls from 1d.js

--- commands/1d.js
+++ commands/1d.js
@@ -6,7 +6,6 @@ exports.run = (client, message, args) => {
 	var parts = content.split(" ");
 	const max = parts[1];
 	var dieResult = Math.floor(Math.random() * (max - min + 1)) + min;
-
 	var dieWeighted = dieResult + 3;
 	var badDie = max - 3;
 	var bonus = parseInt(parts[2]);
@@ -14,39 +13,16 @@ exports.run = (client, message, args) => {
 	const nat20 = 'https://imgur.com/63XO760.png';
 	const nat1 = 'https://imgur.com/fbSf7VJ.gif	';
 
+	if ( isNaN(max) ) {
+		return message.reply(`Use real numbers, fag`);
+	}
+
+	
     if (args < 2) {
         return message.channel.send("Please pick a number greater than 1.");
 
     } else {
 
-        /*if (message.author.id == client.config.renegadeID) {
-
-			if (dieResult <= badDie) {
-
-				const dieEmbed = new Discord.RichEmbed()
-					.setColor(client.config.color)
-					.setAuthor(`${max}-Sided Die.`)
-					.setDescription(`${message.author.username} rolled a(n) **${dieWeighted}**.`)
-				if (bonus > 0) {
-					dieEmbed.addField(`Bonus Total`, `${dieWeighted} + ${bonus} = **${bonusTotal}**`)
-				}
-				return message.channel.send(dieEmbed);
-
-			} else {
-
-				const dieEmbed = new Discord.RichEmbed()
-					.setColor(client.config.color)
-					.setAuthor(`${max}-Sided Die.`)
-					.setDescription(`${message.author.username} rolled a(n) **${dieResult}**.`)
-				if (bonus > 0) {
-					dieEmbed.addField(`Bonus Total`, `${dieResult} + ${bonus} = **${bonusTotal}**`)
-				}
-				return message.channel.send(dieEmbed);
-
-			}
-
-
-		} else {*/
 			if (max == 20) {
 				if (dieResult == 20) {
 
