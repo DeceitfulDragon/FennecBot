112b40d584318a1db95b1721e37ad18e7ea21d37
FearTheRenegade <50032027+FearTheRenegade@users.noreply.github.com>
2019-04-29 16:36:04 -0400

    Made the Rate command
    //rate <subject> and see how FennecBot thinks of it.

---
+++ commands/rate.js
@@ -0,0 +1,10 @@
+﻿
+const max = 10
+const min = 1
+exports.run = (client, message, args) => {
+
+	var rateV = Math.floor(Math.random() * (max - min + 1)) + min;
+
+		return message.channel.send(`I rate that a(n) **${rateV}**.`);
+
+};
