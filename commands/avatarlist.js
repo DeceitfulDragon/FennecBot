62d85e593112e5ea3fd1470001977653543355de
FearTheRenegade <50032027+FearTheRenegade@users.noreply.github.com>
2019-05-01 22:39:21 -0400

    fixed untracked files

--- commands/avatarlist.js
+++
@@ -1,7 +0,0 @@
-﻿exports.run = (client, message) => {
-    const avList = message.mentions.users.map(user => {
-        return `:pushpin: ${user.username}'s avatar: <${user.displayAvatarURL}>`;
-    });
-
-    message.channel.send(avList);
-};
