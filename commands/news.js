f52a40a0ff516889e31c8d1adf7c5af3bf69e5fc
FearTheRenegade <50032027+FearTheRenegade@users.noreply.github.com>
2019-05-03 16:20:19 -0400

    Finished the news command (using NewsAPI)

--- commands/news.js
+++ commands/news.js
@@ -8,219 +8,85 @@ exports.run = (client, message, args) => {
 
 	var content = message.content;
 	var parts = content.split(" ");
-	const cat = parts[1];
-	//const cat = lowcat.toLowerCase();
+	const str = parts[1];
+
+	if (!str) {
+		var cat = parts[1];
+	} else {
+		const lowcat = str.toString();
+		var cat = lowcat.toLowerCase();
+	}
+
+	// IF functions to set the category for the newsAPI
 
 	if (!cat) {
 
-		newsapi.v2.topHeadlines({
-			language: 'en',
-			category: 'general',
-			pageSize: 1,
-			country: 'us'
-		}).then(response => {
+		var category = 'general';
 
-			//var Image = response.articles.map(({ urlToImage }) => urlToImage);
-
-			var Author = response.articles.map(({ author }) => author);
-			var Title = response.articles.map(({ title }) => title);
-			var Description = response.articles.map(({ description }) => description);
-			var URL = response.articles.map(({ url }) => url);
-			var Source = response.articles.map(({ source }) => source);
-			var Site = Source.map(({ name }) => name);
-
-			const newsEmbed = new Discord.RichEmbed()
-				//.setThumbnail(Image)
-				.setTitle(Title)
-				.setColor(client.config.newsColor)
-				.setAuthor(`${Author} on ${Site}`)
-				.setDescription(`${Description} \n ${URL}`)
-			return message.channel.send(newsEmbed);
-
-		});
 	} else if (cat == 'business') {
 
-
-		newsapi.v2.topHeadlines({
-			language: 'en',
-			category: 'business',
-			pageSize: 1,
-			country: 'us'
-		}).then(response => {
-
-			//var Image = response.articles.map(({ urlToImage }) => urlToImage);
-
-			var Author = response.articles.map(({ author }) => author);
-			var Title = response.articles.map(({ title }) => title);
-			var Description = response.articles.map(({ description }) => description);
-			var URL = response.articles.map(({ url }) => url);
-			var Source = response.articles.map(({ source }) => source);
-			var Site = Source.map(({ name }) => name);
-
-			const newsEmbed = new Discord.RichEmbed()
-				//.setThumbnail(Image)
-				.setTitle(Title)
-				.setColor(client.config.newsColor)
-				.setAuthor(`${Author} on ${Site}`)
-				.setDescription(`${Description} \n ${URL}`)
-			return message.channel.send(newsEmbed);
-
-		});
+		var category = 'business';
 
 	} else if (cat == 'technology') {
 
-
-		newsapi.v2.topHeadlines({
-			language: 'en',
-			category: 'technology',
-			pageSize: 1,
-			country: 'us'
-		}).then(response => {
-
-			//var Image = response.articles.map(({ urlToImage }) => urlToImage);
-
-			var Author = response.articles.map(({ author }) => author);
-			var Title = response.articles.map(({ title }) => title);
-			var Description = response.articles.map(({ description }) => description);
-			var URL = response.articles.map(({ url }) => url);
-			var Source = response.articles.map(({ source }) => source);
-			var Site = Source.map(({ name }) => name);
-
-			const newsEmbed = new Discord.RichEmbed()
-				//.setThumbnail(Image)
-				.setTitle(Title)
-				.setColor(client.config.newsColor)
-				.setAuthor(`${Author} on ${Site}`)
-				.setDescription(`${Description} \n ${URL}`)
-			return message.channel.send(newsEmbed);
-
-		});
-
+		var category = 'technology';
 
 	} else if (cat == 'entertainment') {
 
-
-
-		newsapi.v2.topHeadlines({
-			language: 'en',
-			category: 'entertainment',
-			pageSize: 1,
-			country: 'us'
-		}).then(response => {
-
-			//var Image = response.articles.map(({ urlToImage }) => urlToImage);
-
-			var Author = response.articles.map(({ author }) => author);
-			var Title = response.articles.map(({ title }) => title);
-			var Description = response.articles.map(({ description }) => description);
-			var URL = response.articles.map(({ url }) => url);
-			var Source = response.articles.map(({ source }) => source);
-			var Site = Source.map(({ name }) => name);
-
-			const newsEmbed = new Discord.RichEmbed()
-				//.setThumbnail(Image)
-				.setTitle(Title)
-				.setColor(client.config.newsColor)
-				.setAuthor(`${Author} on ${Site}`)
-				.setDescription(`${Description} \n ${URL}`)
-			return message.channel.send(newsEmbed);
-
-		});
+		var category = 'entertainment';
 
 	} else if (cat == 'health') {
 
-
-		newsapi.v2.topHeadlines({
-			language: 'en',
-			category: 'health',
-			pageSize: 1,
-			country: 'us'
-		}).then(response => {
-
-			//var Image = response.articles.map(({ urlToImage }) => urlToImage);
-
-			var Author = response.articles.map(({ author }) => author);
-			var Title = response.articles.map(({ title }) => title);
-			var Description = response.articles.map(({ description }) => description);
-			var URL = response.articles.map(({ url }) => url);
-			var Source = response.articles.map(({ source }) => source);
-			var Site = Source.map(({ name }) => name);
-
-			const newsEmbed = new Discord.RichEmbed()
-				//.setThumbnail(Image)
-				.setTitle(Title)
-				.setColor(client.config.newsColor)
-				.setAuthor(`${Author} on ${Site}`)
-				.setDescription(`${Description} \n ${URL}`)
-			return message.channel.send(newsEmbed);
-
-		});
+		var category = 'health';
 
 	} else if (cat == 'science') {
 
-
-		newsapi.v2.topHeadlines({
-			language: 'en',
-			category: 'science',
-			pageSize: 1,
-			country: 'us'
-		}).then(response => {
-
-			//var Image = response.articles.map(({ urlToImage }) => urlToImage);
-
-			var Author = response.articles.map(({ author }) => author);
-			var Title = response.articles.map(({ title }) => title);
-			var Description = response.articles.map(({ description }) => description);
-			var URL = response.articles.map(({ url }) => url);
-			var Source = response.articles.map(({ source }) => source);
-			var Site = Source.map(({ name }) => name);
-
-			const newsEmbed = new Discord.RichEmbed()
-				//.setThumbnail(Image)
-				.setTitle(Title)
-				.setColor(client.config.newsColor)
-				.setAuthor(`${Author} on ${Site}`)
-				.setDescription(`${Description} \n ${URL}`)
-			return message.channel.send(newsEmbed);
-
-		});
+		var category = 'science';
 
 	} else if (cat == 'sports') {
 
+		var category = 'sports';
 
-		newsapi.v2.topHeadlines({
-			language: 'en',
-			category: 'sports',
-			pageSize: 1,
-			country: 'us'
-		}).then(response => {
+	} else if (cat == 'help') {		// Help Embed for the categories
 
-			//var Image = response.articles.map(({ urlToImage }) => urlToImage);
-
-			var Author = response.articles.map(({ author }) => author);
-			var Title = response.articles.map(({ title }) => title);
-			var Description = response.articles.map(({ description }) => description);
-			var URL = response.articles.map(({ url }) => url);
-			var Source = response.articles.map(({ source }) => source);
-			var Site = Source.map(({ name }) => name);
-
-			const newsEmbed = new Discord.RichEmbed()
-				//.setThumbnail(Image)
-				.setTitle(Title)
-				.setColor(client.config.newsColor)
-				.setAuthor(`${Author} on ${Site}`)
-				.setDescription(`${Description} \n ${URL}`)
-			return message.channel.send(newsEmbed);
-
-		});
-
-	} else if (cat == 'help') {
-
-		const newsEmbed = new Discord.RichEmbed()
+		const helpEmbed = new Discord.RichEmbed()
 			.setTitle(`Available Categories:`)
 			.setColor(client.config.newsColor)
 			.setDescription(`Business\nEntertainment\nHealth\nScience\nSports\nTechnology\n[Don't put any category to get general news.]`)
-		return message.channel.send(newsEmbed);
+		return message.channel.send(helpEmbed);
 
 	}
+
+	// API Time
+
+	newsapi.v2.topHeadlines({
+		language: 'en',
+		category: category,
+		pageSize: 1,
+		country: 'us'
+	}).then(response => {
+
+		// I had to map these all out just to get Discord to recognize it all...
+
+		var img = response.articles.map(({ urlToImage }) => urlToImage);
+		var Image = img.toString();
+		var Author = response.articles.map(({ author }) => author);
+		var Title = response.articles.map(({ title }) => title);
+		var Description = response.articles.map(({ description }) => description);
+		var URL = response.articles.map(({ url }) => url);
+		var Source = response.articles.map(({ source }) => source);
+		var Site = Source.map(({ name }) => name);
+		var betterImage = Image.split("'");
+
+		// finally send the news embed
+
+		const newsEmbed = new Discord.RichEmbed()
+			.setThumbnail(betterImage[0])
+			.setTitle(Title)
+			.setColor(client.config.newsColor)
+			.setAuthor(`${Author} on ${Site}`)
+			.setDescription(`${Description} \n ${URL}`)
+		return message.channel.send(newsEmbed);
+
+	});
 };
