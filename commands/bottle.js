dc120067f642648088b045fd2477fe7f3cef2518
FearTheRenegade <50032027+FearTheRenegade@users.noreply.github.com>
2019-05-21 17:07:03 -0400

    Create bottle.js

---
+++ commands/bottle.js
@@ -0,0 +1,9 @@
+﻿
+
+exports.run = (client, message) => {
+
+	var chosen = message.guild.members.random()
+
+	return message.channel.send(`${message.author} just spun the bottle! It landed on ${chosen}`);
+
+};
